be7838688233fc9e15be1ffdbdd9b8471f98642f
Junior Pinho <juniorpinhodev@gmail.com>
2024-02-14 18:39:13 -0300

    primeiro

---
+++ exercicios/ex001Função_de_mensagem.js
@@ -0,0 +1,5 @@
+function exibirMensagem(mensagem) {
+    console.log(mensagem);
+  }
+  
+  exibirMensagem("Olá, eu sou aluno do Floripa Mais Tec");
---
+++ exercicios/ex003Concatenacao.js
@@ -0,0 +1,7 @@
+const nome = "Jedir";
+const sobrenome = "Junior";
+const turma = "Fitness";
+
+const saudacao = `Olá, ${nome} ${sobrenome}! Você é aluno da turma ${turma} no curso Floripa Mais Tec.`;
+
+console.log(saudacao);
