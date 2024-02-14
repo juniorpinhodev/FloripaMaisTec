4eb9e2bc88b76c98c438666f07ed53e1a8a96fe6
Junior Pinho <juniorpinhodev@gmail.com>
2024-02-14 18:51:50 -0300

    Removendo arquivos de exercícios

--- exercicios/ex001Função_de_mensagem.js
+++
@@ -1,5 +0,0 @@
-function exibirMensagem(mensagem) {
-    console.log(mensagem);
-  }
-  
-  exibirMensagem("Olá, eu sou aluno do Floripa Mais Tec");
--- exercicios/ex003Concatenacao.js
+++
@@ -1,7 +0,0 @@
-const nome = "Jedir";
-const sobrenome = "Junior";
-const turma = "Fitness";
-
-const saudacao = `Olá, ${nome} ${sobrenome}! Você é aluno da turma ${turma} no curso Floripa Mais Tec.`;
-
-console.log(saudacao);
