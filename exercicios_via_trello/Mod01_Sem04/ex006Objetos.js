871a8784c3ab17b988329fcc7059fc608a542de9
Junior Pinho <juniorpinhodev@gmail.com>
2024-02-15 12:19:29 -0300

    add objetos

---
+++ exercicios_via_trello/Mod01_Sem04/ex006Objetos.js
@@ -0,0 +1,39 @@
+let pessoa1 = {
+    nome: "José",
+    idade: 30,
+    nacionalidade: "Brasileiro",
+    profissao: "Desenvolvedor Backend"
+  };
+  
+  let pessoa2 = {
+    nome: "Letícia",
+    idade: 25,
+    nacionalidade: "Brasileira",
+    profissao: "Desenvolvedora Frontend"
+  };
+  
+  let pessoa3 = {
+    nome: "Nicolas",
+    idade: 35,
+    nacionalidade: "Brasileiro",
+    profissao: "Desenvolvedor Fullstack"
+  };
+  
+  console.log("Pessoa 1:");
+  console.log("Nome:", pessoa1.nome);
+  console.log("Idade:", pessoa1.idade);
+  console.log("Nacionalidade:", pessoa1.nacionalidade);
+  console.log("Profissão:", pessoa1.profissao);
+  
+  console.log("Pessoa 2:");
+  console.log("Nome:", pessoa2.nome);
+  console.log("Idade:", pessoa2.idade);
+  console.log("Nacionalidade:", pessoa2.nacionalidade);
+  console.log("Profissão:", pessoa2.profissao);
+  
+  console.log("Pessoa 3:");
+  console.log("Nome:", pessoa3.nome);
+  console.log("Idade:", pessoa3.idade);
+  console.log("Nacionalidade:", pessoa3.nacionalidade);
+  console.log("Profissão:", pessoa3.profissao);
+  
