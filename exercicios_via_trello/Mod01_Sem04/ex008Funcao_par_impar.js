829b7b68d11628ad791891f331d048b612ad8e0c
Junior Pinho <juniorpinhodev@gmail.com>
2024-02-15 14:08:36 -0300

    add exercicio 8

---
+++ exercicios_via_trello/Mod01_Sem04/ex008Funcao_par_impar.js
@@ -0,0 +1,12 @@
+// Implemente uma função que determine se um número é par ou ímpar.
+
+function parOuImpar(numero) {
+    if (numero % 2 === 0) {
+      console.log("O número é par.");
+    } else {
+      console.log("O número é ímpar.");
+    }
+  }
+  
+  parOuImpar(5);
+  parOuImpar(8);
