24aef14496464b68a778481f8dbe9066a70dc4c9
Junior Pinho <juniorpinhodev@gmail.com>
2024-02-27 19:18:30 -0300

    atualizações e melhorias

--- exercicios_via_trello/Mod01_Sem06/Perfil_usuario/main.js
+++ exercicios_via_trello/Mod01_Sem06/Perfil_usuario/main.js
@@ -1,9 +1,26 @@
 function mostrarMensagem() {
     alert('Bem-vindo(a) ao Perfil do Usuário!');
 }
-
 mostrarMensagem();
 
 const tituloPerfil = document.querySelector('.perfil-usuario h2');
 tituloPerfil.style.color = 'blue';
+
+function handleButtonActions() {
+    const editButton = document.querySelector('.botao.editar');
+    const passwordButton = document.querySelector('.botao.senha');
+  
+    editButton.addEventListener('click', () => {
+      // Ação para editar o perfil
+      alert('Clicou em Editar Perfil!');
+    });
+  
+    passwordButton.addEventListener('click', () => {
+      // Ação para alterar a senha
+      alert('Clicou em Alterar Senha!');
+    });
+  }
+  
+  handleButtonActions();
+  
 
