021f0a56ebab74428f4028a6d6d3b9b9a833c654
Junior Pinho <juniorpinhodev@gmail.com>
2024-03-11 22:40:01 -0300

    exercicio 6

--- exercicios_via_trello/Mod01_Sem08/script.js
+++ exercicios_via_trello/Mod01_Sem08/script.js
@@ -1,13 +1,7 @@
+// Seleciona o elemento pelo ID
 let elemento = document.getElementById("meuElemento");
-    
-elemento.style.backgroundColor = "red";
 
-
-let elementos = document.getElementsByClassName("minhaClasse");
-
-for (let i = 0; i < elementos.length; i++) {
-    elementos[i].style.color = "blue";
-    elementos[i].style.fontWeight = "bold";
-}
+// Adiciona a nova classe ao elemento
+elemento.classList.add("minhaNovaClasse");
 
 
