60b454631c397a16e40e35573ecaadded0f7bef2
Junior Pinho <juniorpinhodev@gmail.com>
2024-03-11 22:17:13 -0300

    exercicio 3

--- exercicios_via_trello/Mod01_Sem08/script.js
+++ exercicios_via_trello/Mod01_Sem08/script.js
@@ -8,3 +8,16 @@ let elementos = document.getElementsByClassName("minhaClasse");
 for (let i = 0; i < elementos.length; i++) {
     elementos[i].textContent = "Novo texto do elemento " + (i + 1);
 }
+
+
+// Cria um novo elemento <p>
+var novoElemento = document.createElement("p");
+
+// Adiciona texto ao novo elemento
+novoElemento.textContent = "Este é um novo parágrafo";
+
+// Seleciona o elemento pai 
+var elementoPai = document.getElementById("meuElemento");
+
+// Adiciona o novo elemento como filho do elemento pai
+elementoPai.appendChild(novoElemento);
