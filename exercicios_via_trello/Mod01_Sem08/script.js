2022e46b3b0be59d1aece5adbfd073526f8663aa
Junior Pinho <juniorpinhodev@gmail.com>
2024-03-11 22:12:37 -0300

    exercicio 2

--- exercicios_via_trello/Mod01_Sem08/script.js
+++ exercicios_via_trello/Mod01_Sem08/script.js
@@ -1,3 +1,10 @@
 let elemento = document.getElementById("meuElemento");
     
 elemento.style.backgroundColor = "red";
+
+
+let elementos = document.getElementsByClassName("minhaClasse");
+
+for (let i = 0; i < elementos.length; i++) {
+    elementos[i].textContent = "Novo texto do elemento " + (i + 1);
+}
