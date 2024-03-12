5c9cebe901445147a9d8474a83cd5ec75b54488f
Junior Pinho <juniorpinhodev@gmail.com>
2024-03-11 23:15:00 -0300

    ToDo List - Lista de tarefas

--- exercicios_via_trello/Mod01_Sem08/script.js
+++ exercicios_via_trello/Mod01_Sem08/script.js
@@ -1,8 +1,49 @@
-// Seleciona o elemento pelo ID
-let myDiv = document.getElementById('myDiv');
-        
-// Remove o atributo 'data-custom'
-myDiv.removeAttribute('data-custom');
+let tasks = [];
 
-// Verifica se o atributo foi removido
-console.log(myDiv.getAttribute('data-custom')); 
+function renderTasks() {
+    const taskList = document.getElementById('taskList');
+    taskList.innerHTML = '';
+    
+    tasks.forEach((task, index) => {
+      const li = document.createElement('li');
+      li.textContent = task.text;
+      
+      if (task.completed) {
+        li.classList.add('completed');
+      }
+      
+      const completeButton = document.createElement('button');
+      completeButton.textContent = 'Concluir';
+      completeButton.className = 'complete-button'; // Adicione a classe ao botão
+      completeButton.onclick = () => {
+        tasks[index].completed = !tasks[index].completed;
+        renderTasks();
+      };
+      
+      const deleteButton = document.createElement('button');
+      deleteButton.textContent = 'Remover';
+      deleteButton.onclick = () => {
+        tasks.splice(index, 1);
+        renderTasks();
+      };
+      
+      li.appendChild(completeButton);
+      li.appendChild(deleteButton);
+      
+      taskList.appendChild(li);
+    });
+  }
+  
+
+function addTask() {
+  const taskInput = document.getElementById('taskInput');
+  const taskText = taskInput.value.trim();
+  
+  if (taskText) {
+    tasks.push({ text: taskText, completed: false });
+    taskInput.value = '';
+    renderTasks();
+  }
+}
+
+renderTasks();
