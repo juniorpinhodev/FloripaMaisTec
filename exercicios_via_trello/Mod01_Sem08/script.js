960eee72f5f9411f675a0bdd5aa1d2473dba5499
Junior Pinho <juniorpinhodev@gmail.com>
2024-03-11 22:44:29 -0300

    exercicio 7

--- exercicios_via_trello/Mod01_Sem08/script.js
+++ exercicios_via_trello/Mod01_Sem08/script.js
@@ -1,7 +1,6 @@
 // Seleciona o elemento pelo ID
 let elemento = document.getElementById("meuElemento");
 
-// Adiciona a nova classe ao elemento
-elemento.classList.add("minhaNovaClasse");
-
+// Remove a classe do elemento
+elemento.classList.remove("minhaClasse");
 
