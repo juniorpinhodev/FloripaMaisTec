bc9fe2522fdc67089bcc61edc021362c0c66be18
Junior Pinho <juniorpinhodev@gmail.com>
2024-03-11 21:10:00 -0300

    exercicio 4

--- exercicios_via_trello/Mod01_Sem05/Carrinho_de_compras/script.js
+++ exercicios_via_trello/Mod01_Sem05/Carrinho_de_compras/script.js
@@ -3,9 +3,11 @@ const produtos = {
     "arroz": 10.00,
     "feijão": 8.00,
     "macarrão": 5.00,
-    "café": 7.50
+    "café": 7.50,
+    "leite": 3.49,
+    "pão": 5.50,
+    
 };
-
 
 // Carrinho de compras do usuário
 let carrinho = [];
@@ -27,5 +29,18 @@ function comprarProduto() {
         alert(`Produto "${produtoInput}" adicionado ao carrinho.`);
     } else {
         alert(`Produto "${produtoInput}" não encontrado.`);
+    }
+}
+
+function calcularTotal() {
+    let total = 0;
+    carrinho.forEach(item => {
+        total += item.preco;
+    });
+
+    if (total > 0) {
+        document.getElementById('total').innerText = `Total da compra: R$ ${total.toFixed(2)}`;
+    } else {
+        document.getElementById('total').innerText = 'Carrinho vazio';
     }
 }
